9f0ff27446d7265a2ff1212ad08769de2669efcd
Jinia <afrosejnu@gmail.com>
2023-03-13 21:17:18 -0500

    new practice file added

--- alert/script.js
+++ alert/script.js
@@ -7,3 +7,8 @@ console.log(`You are ${age} years old`);
 let isBoss = confirm("Are you the boss?");
 
 alert(isBoss); // true if OK is pressed
+
+//let adult = age > 18 ? true : false;
+age = prompt("What is your age?", 18);
+let adult = age > 18;
+alert(adult);
